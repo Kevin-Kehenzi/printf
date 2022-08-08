10bbe624f8b1d7588e31fc88dd55aa5ef4528051
Gitau01Frank <franciswainainag@gmail.com>
2022-08-08 15:02:04 +0300

    Committing main.h file

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdio.h>
+int _printf(const char *format, ...);
+
+
+#endif
