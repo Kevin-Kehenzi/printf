90b79dc35f114b400679b2ac303ee2c5ee8c4f84
Gitau01Frank <franciswainainag@gmail.com>
2022-08-08 16:52:04 +0300

    _printf commits

--- main.h
+++ main.h
@@ -1,13 +1,8 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
-#include <stdarg.h>
-#include <stdlib.h>
-#include <stddef.h>
-#include <unistd.h>
-#include <limits.h>
-#include <stdio.h>
 int _printf(const char *format, ...);
+int _putchar(char c); 
 
 
 #endif
